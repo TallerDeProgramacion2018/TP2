6d9d4fc150633eecbe5ed35dedf9cbf0c9f85e90
nacho <ignaciodentti@gmail.com>
2018-08-24 17:52:22 -0300

    Hols

---
+++ TP2/EJ 01/Punto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ_01
+{
+    class Punto
+    {
+        private double iX;
+        private double iY;
+
+        public Punto(double pX, double pY)
+        {
+            this.iX = pX;
+            this.iY = pY;
+        }
+
+        public double X
+        {
+            get { return this.iX; }
+            set { this.iX = value; }
+        }
+
+        public double Y
+        {
+            get { return this.iY; }
+            set { this.iY = value; }
+        }
+
+        public double CalcularDistanciaDesde(Punto pPunto)
+        {
+            return Math.Sqrt(Math.Pow(this.iX - pPunto.iX, 2) + Math.Pow(this.iY - pPunto.iY, 2));
+        }
+    }
+}
---
+++ TP2/EJ 01/Triangulo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ_01
+{
+    class Triangulo
+    {
+        private Punto iPunto1;
+        private Punto iPunto2;
+        private Punto iPunto3;
+
+        public Triangulo(Punto pPunto1, Punto pPunto2, Punto pPunto3)
+        {
+            this.iPunto1 = pPunto1;
+            this.iPunto2 = pPunto2;
+            this.iPunto3 = pPunto3;
+        }
+
+        public Punto Punto1
+        {
+            get { return this.iPunto1; }
+            set { this.iPunto1 = value; }
+        }
+
+        public Punto Punto2
+        {
+            get { return this.iPunto2; }
+            set { this.iPunto2 = value; }
+        }
+
+        public Punto Punto3
+        {
+            get { return this.iPunto3; }
+            set { this.iPunto3 = value; }
+        }
+
+        public double Perimetro
+        {
+            get { return (iPunto1.CalcularDistanciaDesde(iPunto2) + iPunto1.CalcularDistanciaDesde(iPunto3) + iPunto2.CalcularDistanciaDesde(iPunto3)); }
+        }
+
+        public double Area
+        {
+            get
+            {
+                double semip = this.Perimetro / 2;
+                return Math.Sqrt(semip * (semip - iPunto1.CalcularDistanciaDesde(iPunto2)) * (semip - iPunto1.CalcularDistanciaDesde(iPunto3)) * (semip - iPunto2.CalcularDistanciaDesde(iPunto3)));
+            }
+        }
+    }
+}
+    
